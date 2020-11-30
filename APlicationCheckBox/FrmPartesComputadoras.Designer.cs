d8bacb187a2367cf143ff2067625ec1d76b66d8b
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2020-11-30 10:55:19 -0500

    Agregar archivos de proyecto.

---
+++ APlicationCheckBox/FrmPartesComputadoras.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace APlicationCheckBox
+{
+    partial class FrmPartesComputadoras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton4GB = new System.Windows.Forms.RadioButton();
+            this.radioButton8GB = new System.Windows.Forms.RadioButton();
+            this.radioButton12GB = new System.Windows.Forms.RadioButton();
+            this.radioButton80SOLIDO = new System.Windows.Forms.RadioButton();
+            this.radioButton300SOLIDO = new System.Windows.Forms.RadioButton();
+            this.radioButton500SOLIDO = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButtonIP3 = new System.Windows.Forms.RadioButton();
+            this.radioButtonIP5 = new System.Windows.Forms.RadioButton();
+            this.radioButtonIP7 = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtComponenetes = new System.Windows.Forms.TextBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.checkBoxCAMWEB = new System.Windows.Forms.CheckBox();
+            this.checkBoxIMPRESORA = new System.Windows.Forms.CheckBox();
+            this.checkBoxMOUSEGAMING = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReverse = new System.Windows.Forms.Button();
+            this.txtEnterofour = new System.Windows.Forms.TextBox();
+            this.txtReverse = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton12GB);
+            this.groupBox1.Controls.Add(this.radioButton8GB);
+            this.groupBox1.Controls.Add(this.radioButton4GB);
+            this.groupBox1.Location = new System.Drawing.Point(12, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 145);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Memoria RAM";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton500SOLIDO);
+            this.groupBox2.Controls.Add(this.radioButton300SOLIDO);
+            this.groupBox2.Controls.Add(this.radioButton80SOLIDO);
+            this.groupBox2.Location = new System.Drawing.Point(237, 61);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 145);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ALMACENAMIENTO";
+            // 
+            // radioButton4GB
+            // 
+            this.radioButton4GB.AutoSize = true;
+            this.radioButton4GB.Location = new System.Drawing.Point(19, 31);
+            this.radioButton4GB.Name = "radioButton4GB";
+            this.radioButton4GB.Size = new System.Drawing.Size(49, 17);
+            this.radioButton4GB.TabIndex = 0;
+            this.radioButton4GB.TabStop = true;
+            this.radioButton4GB.Text = "4 GB";
+            this.radioButton4GB.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8GB
+            // 
+            this.radioButton8GB.AutoSize = true;
+            this.radioButton8GB.Location = new System.Drawing.Point(19, 64);
+            this.radioButton8GB.Name = "radioButton8GB";
+            this.radioButton8GB.Size = new System.Drawing.Size(49, 17);
+            this.radioButton8GB.TabIndex = 1;
+            this.radioButton8GB.TabStop = true;
+            this.radioButton8GB.Text = "8 GB";
+            this.radioButton8GB.UseVisualStyleBackColor = true;
+            // 
+            // radioButton12GB
+            // 
+            this.radioButton12GB.AutoSize = true;
+            this.radioButton12GB.Location = new System.Drawing.Point(19, 104);
+            this.radioButton12GB.Name = "radioButton12GB";
+            this.radioButton12GB.Size = new System.Drawing.Size(55, 17);
+            this.radioButton12GB.TabIndex = 2;
+            this.radioButton12GB.TabStop = true;
+            this.radioButton12GB.Text = "12 GB";
+            this.radioButton12GB.UseVisualStyleBackColor = true;
+            // 
+            // radioButton80SOLIDO
+            // 
+            this.radioButton80SOLIDO.AutoSize = true;
+            this.radioButton80SOLIDO.Location = new System.Drawing.Point(16, 31);
+            this.radioButton80SOLIDO.Name = "radioButton80SOLIDO";
+            this.radioButton80SOLIDO.Size = new System.Drawing.Size(98, 17);
+            this.radioButton80SOLIDO.TabIndex = 0;
+            this.radioButton80SOLIDO.TabStop = true;
+            this.radioButton80SOLIDO.Text = "80 GB SOLIDO";
+            this.radioButton80SOLIDO.UseVisualStyleBackColor = true;
+            // 
+            // radioButton300SOLIDO
+            // 
+            this.radioButton300SOLIDO.AutoSize = true;
+            this.radioButton300SOLIDO.Location = new System.Drawing.Point(16, 64);
+            this.radioButton300SOLIDO.Name = "radioButton300SOLIDO";
+            this.radioButton300SOLIDO.Size = new System.Drawing.Size(104, 17);
+            this.radioButton300SOLIDO.TabIndex = 2;
+            this.radioButton300SOLIDO.TabStop = true;
+            this.radioButton300SOLIDO.Text = "300 GB SOLIDO";
+            this.radioButton300SOLIDO.UseVisualStyleBackColor = true;
+            // 
+            // radioButton500SOLIDO
+            // 
+            this.radioButton500SOLIDO.AutoSize = true;
+            this.radioButton500SOLIDO.Location = new System.Drawing.Point(16, 104);
+            this.radioButton500SOLIDO.Name = "radioButton500SOLIDO";
+            this.radioButton500SOLIDO.Size = new System.Drawing.Size(104, 17);
+            this.radioButton500SOLIDO.TabIndex = 4;
+            this.radioButton500SOLIDO.TabStop = true;
+            this.radioButton500SOLIDO.Text = "500 GB SOLIDO";
+            this.radioButton500SOLIDO.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButtonIP7);
+            this.groupBox3.Controls.Add(this.radioButtonIP5);
+            this.groupBox3.Controls.Add(this.radioButtonIP3);
+            this.groupBox3.Location = new System.Drawing.Point(468, 61);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 145);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "PROCESADOR";
+            // 
+            // radioButtonIP3
+            // 
+            this.radioButtonIP3.AutoSize = true;
+            this.radioButtonIP3.Location = new System.Drawing.Point(17, 31);
+            this.radioButtonIP3.Name = "radioButtonIP3";
+            this.radioButtonIP3.Size = new System.Drawing.Size(98, 17);
+            this.radioButtonIP3.TabIndex = 1;
+            this.radioButtonIP3.TabStop = true;
+            this.radioButtonIP3.Text = "Intel Pentium I3";
+            this.radioButtonIP3.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonIP5
+            // 
+            this.radioButtonIP5.AutoSize = true;
+            this.radioButtonIP5.Location = new System.Drawing.Point(17, 64);
+            this.radioButtonIP5.Name = "radioButtonIP5";
+            this.radioButtonIP5.Size = new System.Drawing.Size(98, 17);
+            this.radioButtonIP5.TabIndex = 3;
+            this.radioButtonIP5.TabStop = true;
+            this.radioButtonIP5.Text = "Intel Pentium I5";
+            this.radioButtonIP5.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonIP7
+            // 
+            this.radioButtonIP7.AutoSize = true;
+            this.radioButtonIP7.Location = new System.Drawing.Point(17, 104);
+            this.radioButtonIP7.Name = "radioButtonIP7";
+            this.radioButtonIP7.Size = new System.Drawing.Size(98, 17);
+            this.radioButtonIP7.TabIndex = 5;
+            this.radioButtonIP7.TabStop = true;
+            this.radioButtonIP7.Text = "Intel Pentium I7";
+            this.radioButtonIP7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.checkBoxMOUSEGAMING);
+            this.groupBox4.Controls.Add(this.checkBoxIMPRESORA);
+            this.groupBox4.Controls.Add(this.checkBoxCAMWEB);
+            this.groupBox4.Location = new System.Drawing.Point(705, 61);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 145);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "ACCESORIOS";
+            // 
+            // txtComponenetes
+            // 
+            this.txtComponenetes.Location = new System.Drawing.Point(12, 226);
+            this.txtComponenetes.Multiline = true;
+            this.txtComponenetes.Name = "txtComponenetes";
+            this.txtComponenetes.ReadOnly = true;
+            this.txtComponenetes.Size = new System.Drawing.Size(893, 63);
+            this.txtComponenetes.TabIndex = 4;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(12, 295);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(93, 42);
+            this.btnProcesar.TabIndex = 5;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // checkBoxCAMWEB
+            // 
+            this.checkBoxCAMWEB.AutoSize = true;
+            this.checkBoxCAMWEB.Location = new System.Drawing.Point(17, 31);
+            this.checkBoxCAMWEB.Name = "checkBoxCAMWEB";
+            this.checkBoxCAMWEB.Size = new System.Drawing.Size(85, 17);
+            this.checkBoxCAMWEB.TabIndex = 5;
+            this.checkBoxCAMWEB.Text = "Camera web";
+            this.checkBoxCAMWEB.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIMPRESORA
+            // 
+            this.checkBoxIMPRESORA.AutoSize = true;
+            this.checkBoxIMPRESORA.Location = new System.Drawing.Point(17, 65);
+            this.checkBoxIMPRESORA.Name = "checkBoxIMPRESORA";
+            this.checkBoxIMPRESORA.Size = new System.Drawing.Size(72, 17);
+            this.checkBoxIMPRESORA.TabIndex = 6;
+            this.checkBoxIMPRESORA.Text = "Impresora";
+            this.checkBoxIMPRESORA.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMOUSEGAMING
+            // 
+            this.checkBoxMOUSEGAMING.AutoSize = true;
+            this.checkBoxMOUSEGAMING.Location = new System.Drawing.Point(17, 105);
+            this.checkBoxMOUSEGAMING.Name = "checkBoxMOUSEGAMING";
+            this.checkBoxMOUSEGAMING.Size = new System.Drawing.Size(97, 17);
+            this.checkBoxMOUSEGAMING.TabIndex = 7;
+            this.checkBoxMOUSEGAMING.Text = "Mouse Gaming";
+            this.checkBoxMOUSEGAMING.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ingreso un entero de 4 numeros";
+            // 
+            // btnReverse
+            // 
+            this.btnReverse.Location = new System.Drawing.Point(22, 400);
+            this.btnReverse.Name = "btnReverse";
+            this.btnReverse.Size = new System.Drawing.Size(75, 23);
+            this.btnReverse.TabIndex = 7;
+            this.btnReverse.Text = "Reverse";
+            this.btnReverse.UseVisualStyleBackColor = true;
+            this.btnReverse.Click += new System.EventHandler(this.btnReverse_Click);
+            // 
+            // txtEnterofour
+            // 
+            this.txtEnterofour.Location = new System.Drawing.Point(198, 362);
+            this.txtEnterofour.Name = "txtEnterofour";
+            this.txtEnterofour.Size = new System.Drawing.Size(125, 20);
+            this.txtEnterofour.TabIndex = 8;
+            // 
+            // txtReverse
+            // 
+            this.txtReverse.Location = new System.Drawing.Point(139, 400);
+            this.txtReverse.Name = "txtReverse";
+            this.txtReverse.Size = new System.Drawing.Size(100, 20);
+            this.txtReverse.TabIndex = 9;
+            // 
+            // FrmPartesComputadoras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 497);
+            this.Controls.Add(this.txtReverse);
+            this.Controls.Add(this.txtEnterofour);
+            this.Controls.Add(this.btnReverse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.txtComponenetes);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmPartesComputadoras";
+            this.Text = "Componentes";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton12GB;
+        private System.Windows.Forms.RadioButton radioButton8GB;
+        private System.Windows.Forms.RadioButton radioButton4GB;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton500SOLIDO;
+        private System.Windows.Forms.RadioButton radioButton300SOLIDO;
+        private System.Windows.Forms.RadioButton radioButton80SOLIDO;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButtonIP7;
+        private System.Windows.Forms.RadioButton radioButtonIP5;
+        private System.Windows.Forms.RadioButton radioButtonIP3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtComponenetes;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.CheckBox checkBoxMOUSEGAMING;
+        private System.Windows.Forms.CheckBox checkBoxIMPRESORA;
+        private System.Windows.Forms.CheckBox checkBoxCAMWEB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReverse;
+        private System.Windows.Forms.TextBox txtEnterofour;
+        private System.Windows.Forms.TextBox txtReverse;
+    }
+}
---
+++ APlicationCheckBox/FrmPartesComputadoras.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APlicationCheckBox
+{
+    public partial class FrmPartesComputadoras : Form
+    {
+        public FrmPartesComputadoras()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            string cad = "";
+            if(this.radioButton4GB.Checked)
+            {
+                cad = "Ordenador with 4GB-RAM";
+            }
+            else if(this.radioButton8GB.Checked)
+            {
+                cad = "Ordenador with 8GB-RAM";
+            }
+            else if (this.radioButton12GB.Checked)
+            {
+                cad = "Ordenador with 12GB-RAM";
+            }
+            //ALMACENAMIENTO
+            string cad2 = "";
+            if (this.radioButton80SOLIDO.Checked)
+            {
+                cad2 = ",with 80GB SOLIDO";
+            }
+            else if (this.radioButton300SOLIDO.Checked)
+            {
+                cad2 = ",with 300GB SOLIDO";
+            }
+            else if (this.radioButton500SOLIDO.Checked)
+            {
+                cad2 = ",with 500GB SOLIDO";
+            }
+            //PROCESADOR
+            string cad3 = "";
+            if (this.radioButtonIP3.Checked)
+            {
+                cad3 = ",with Intel Pentium i3";
+            }
+            else if (this.radioButtonIP5.Checked)
+            {
+                cad3 = ",with Intel Pentium i5";
+            }
+            else if (this.radioButtonIP7.Checked)
+            {
+                cad3 = ",with Intel Pentium i7";
+            }
+            //ACCESORIOS
+            string cad4 = "";
+            if (this.checkBoxCAMWEB.Checked)
+            {
+                cad4 = ",with Camera Web";
+            }
+            string cad5 = "";
+            if (this.checkBoxIMPRESORA.Checked)
+            {
+                cad5 = ",with Impresora";
+            }
+            string cad6 = "";
+            if (this.checkBoxMOUSEGAMING.Checked)
+            {
+                cad6 = ",with Mouse Gaming";
+            }
+            this.txtComponenetes.Text = cad + cad2 + cad3 + cad4 + cad5 + cad6 ;
+            
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            int number = Convert.ToInt32(txtEnterofour.Text);
+            int rever = reverso(number);
+            txtReverse.Text = rever.ToString();
+        }
+        int reverso(int number)
+        {
+            int inverso = 0;
+            while(number > 0)
+            {
+                inverso = inverso * 10 + number % 10;
+                number = number / 10;
+            }
+            return inverso;
+        }
+    }
+}
